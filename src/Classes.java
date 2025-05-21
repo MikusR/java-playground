79c3415bd70dd957b7979e4fe097bbea5b34a4c5
Mikus Rakis <mikus.rakis@gmail.com>
2025-05-21 23:40:47 +0300

    class extends class

---
+++ src/Classes.java
@@ -0,0 +1,50 @@
+public class Classes {
+    public static void main(String[] args) {
+        Knife knife1 = new Classes.Knife();
+        Knife knife2 = new Classes.Knife("maker1", 3);
+        Knife knife3 = new Classes.SwissKnife("maker2", 4, 54);
+        System.out.println(knife1);
+        System.out.println(knife2);
+        System.out.println(knife3);
+    }
+
+    public static class Knife {
+        String maker;
+        int length;
+
+        public Knife() {
+            this("none", 1);
+        }
+
+        public Knife(String maker, int length) {
+            this.maker = maker;
+            this.length = length;
+        }
+
+        @Override
+        public String toString() {
+            return "This is a Knife made by " + this.maker + " and is " + this.length + " long";
+        }
+    }
+
+    public static class SwissKnife extends Knife {
+        int functions;
+
+        public SwissKnife() {
+            super();
+            this.functions = 0;
+        }
+
+        public SwissKnife(String maker, int length, int functions) {
+            super(maker, length);
+            this.functions = functions;
+        }
+
+        @Override
+        public String toString() {
+            return "This is a SwissKnife made by " + this.maker +
+                    ", has " + this.functions +
+                    " functions and is " + this.length + " long";
+        }
+    }
+}
