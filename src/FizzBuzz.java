448818b7e396a44637126eacee12573a86b5ce56
Mikus Rakis <mikus.rakis@gmail.com>
2025-05-21 12:43:49 +0300

    FizzBuzz with StringBuilder

---
+++ src/FizzBuzz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("number");
+        int number = scanner.nextInt();
+        for (int i = 1; i < number; i++) {
+            StringBuilder result = new StringBuilder();
+            if (i % 3 == 0) {
+                result.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                result.append("Buzz");
+            }
+            if (result.toString().isEmpty()) {
+                result.append(i);
+            }
+            System.out.print(i + " ");
+            System.out.println(result);
+        }
+    }
+}
