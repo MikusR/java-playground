368796f8de1d1d26f0fe067b1f4ce66e57a0a68b
Mikus Rakis <mikus.rakis@gmail.com>
2025-05-22 14:23:43 +0300

    Multiple classes same file, but not in single class

--- src/Classes.java
+++ src/Classes.java
@@ -1,50 +1,51 @@
 public class Classes {
+
     public static void main(String[] args) {
-        Knife knife1 = new Classes.Knife();
-        Knife knife2 = new Classes.Knife("maker1", 3);
-        Knife knife3 = new Classes.SwissKnife("maker2", 4, 54);
+        Knife knife1 = new Knife();
+        Knife knife2 = new Knife("maker1", 3);
+        Knife knife3 = new SwissKnife("maker2", 4, 54);
         System.out.println(knife1);
         System.out.println(knife2);
         System.out.println(knife3);
     }
+}
 
-    public static class Knife {
-        String maker;
-        int length;
+class Knife {
+    String maker;
+    int length;
 
-        public Knife() {
-            this("none", 1);
-        }
-
-        public Knife(String maker, int length) {
-            this.maker = maker;
-            this.length = length;
-        }
-
-        @Override
-        public String toString() {
-            return "This is a Knife made by " + this.maker + " and is " + this.length + " long";
-        }
+    public Knife() {
+        this("none", 1);
     }
 
-    public static class SwissKnife extends Knife {
-        int functions;
+    public Knife(String maker, int length) {
+        this.maker = maker;
+        this.length = length;
+    }
 
-        public SwissKnife() {
-            super();
-            this.functions = 0;
-        }
+    @Override
+    public String toString() {
+        return "This is a Knife made by " + this.maker + " and is " + this.length + " long";
+    }
+}
 
-        public SwissKnife(String maker, int length, int functions) {
-            super(maker, length);
-            this.functions = functions;
-        }
+class SwissKnife extends Knife {
+    int functions;
 
-        @Override
-        public String toString() {
-            return "This is a SwissKnife made by " + this.maker +
-                    ", has " + this.functions +
-                    " functions and is " + this.length + " long";
-        }
+    public SwissKnife() {
+        super();
+        this.functions = 0;
+    }
+
+    public SwissKnife(String maker, int length, int functions) {
+        super(maker, length);
+        this.functions = functions;
+    }
+
+    @Override
+    public String toString() {
+        return "This is a SwissKnife made by " + this.maker +
+                ", has " + this.functions +
+                " functions and is " + this.length + " long";
     }
 }
