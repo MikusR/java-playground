fbe4ff05c0d88af5a3073db2ca79af6b1adfd02c
Mikus Rakis <mikus.rakis@gmail.com>
2025-05-22 21:17:35 +0300

    hyperskill ComplexNumber

---
+++ src/Hyperskill/CaO/ComplexNumber.java
@@ -0,0 +1,35 @@
+package Hyperskill.CaO;
+
+class Complex {
+
+    double real;
+    double image;
+
+    public void add(Complex anotherNumber) {
+        this.real += anotherNumber.real;
+        this.image += anotherNumber.image;
+    }
+
+    public void subtract(Complex anotherNumber) {
+        this.real -= anotherNumber.real;
+        this.image -= anotherNumber.image;
+    }
+
+}
+
+public class ComplexNumber {
+    public static void main(String[] args) {
+        Complex number = new Complex();
+        number.real = 10;
+        number.image = 4;
+        // math expression of number = 10.0 + 4.0i
+
+        Complex anotherNumber = new Complex();
+        anotherNumber.real = 6;
+        anotherNumber.image = 6;
+        // math expression of anotherNumber = 6.0 + 6.0i
+
+        number.add(anotherNumber); // number = 16.0 + 10.0i
+    }
+
+}
