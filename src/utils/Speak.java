e491148d4c594a54eeea3fb168d5622999c317c5
Mikus Rakis <mikus.rakis@gmail.com>
2025-05-23 10:09:06 +0300

    Hello

--- src/utils/Speak.java
+++ src/utils/Speak.java
@@ -4,4 +4,8 @@ public class Speak {
     public static void bark() {
         System.out.println("Bark Bark");
     }
+
+    public static void sayHello() {
+        System.out.println("Hello");
+    }
 }
