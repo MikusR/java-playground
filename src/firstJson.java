e6d89d765af61a375f79423744de0de968cc6076
Mikus Rakis <mikus.rakis@gmail.com>
2025-05-23 10:49:57 +0300

    json without json parsing as java has no built in json

---
+++ src/firstJson.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+public class firstJson {
+    public static void main(String[] args) throws IOException {
+        try {
+            URL url = new URL("https://catfact.ninja/fact");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println(responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+
+                System.out.println(response);
+            } else {
+                System.out.println("GET request did not work.");
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
+
