95fefe8118b8e683c46158f41346e17772827965
Mikus Rakis <mikus.rakis@gmail.com>
2025-06-03 22:03:25 +0300

    jackson

---
+++ src/SecondJson.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SecondJson {
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Test test = new Test();
+        String json = mapper.writeValueAsString(test);
+        System.out.println("Serialized JSON: " + json);
+    }
+
+}
+
+class Test {
+    public String a = "aaaa";
+    public String b = "bbbb";
+
+    public Test() {
+    }
+}
