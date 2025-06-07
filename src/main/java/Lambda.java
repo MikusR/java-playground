1b4bce84e6f561c431c9d802435ef9fa5a17ff27
Mikus Rakis <mikus.rakis@gmail.com>
2025-06-07 11:35:18 +0300

    Labda

---
+++ src/main/java/Lambda.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Lambda {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+ scanner.close();
+        
+        Function<Integer, Boolean> function = x -> x % 15 == 0;
+        System.out.println(function.apply(n));
+    }
+}
