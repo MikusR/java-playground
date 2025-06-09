67d4600aedb3e88fe1666aa16958fe70a24767e4
Mikus Rakis <mikus.rakis@gmail.com>
2025-06-09 20:52:31 +0300

    arrays2

---
+++ app/src/main/java/info/mikusr/Arrays2.java
@@ -0,0 +1,16 @@
+package info.mikusr;
+
+import java.util.ArrayList;
+
+public class Arrays2 {
+    public static void main(String[] need) {
+        ArrayList<String> alistOne = new ArrayList<>();
+        alistOne.add("ev1");
+        alistOne.add("ev2");
+        alistOne.add("ev3");
+        System.out.println(alistOne);
+        System.out.println(alistOne.getLast());
+        alistOne.remove("ev2");
+        System.out.println(alistOne);
+    }
+}
