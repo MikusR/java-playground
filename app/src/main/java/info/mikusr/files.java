71962c279d9110f5cb1cd524e4fb6abe01739001
Mikus Rakis <mikus.rakis@gmail.com>
2025-07-01 18:40:26 +0300

    files

--- app/src/main/java/info/mikusr/files.java
+++ app/src/main/java/info/mikusr/files.java
@@ -1,10 +1,18 @@
 package info.mikusr;
 
 import java.io.File;
+import java.io.IOException;
 
 public class files {
     public static void main(String[] arg) {
-        File file = new File("files/file.file");
+        File directory = new File("testfiles");
+        File file = new File("testfiles/file.file");
+        Boolean dirCreated = directory.mkdir();
+        if (dirCreated) {
+            System.out.println("directory was successfully created.");
+        } else {
+            System.out.println("directory was not created.");
+        }
         System.out.println(File.separator);
         System.out.println("File name: " + file.getName());
         System.out.println("File path: " + file.getPath());
@@ -12,5 +20,16 @@ public class files {
         System.out.println("Is directory: " + file.isDirectory());
         System.out.println("Exists: " + file.exists());
         System.out.println("Parent path: " + file.getParent());
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("created " + file.getName());
+            } else {
+                System.out.println("already exists");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
